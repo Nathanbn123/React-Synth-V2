82fd1f5945e92d73eec8b6c4999e45968d02bf5e
Nate Wessels <nwessels16@gmail.com>
2019-05-13 11:06:19 -0700

    on button click plays sound

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,14 +5,19 @@ class Board extends React.Component {
 
   constructor(props) {
     super(props);
-    var synth = new Tone.Synth().toMaster();
 
+  }
+
+  playSound() {
+    var synth = new Tone.Synth().toMaster();
     synth.triggerAttackRelease("C4", "8n");
 
   }
   render() {
     return (
-      <div></div>
+      <div>
+        <button onClick={this.playSound} />
+      </div>
 
     )
   }
